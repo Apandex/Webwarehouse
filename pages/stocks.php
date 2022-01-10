fa87592d45361d63e2468455d2c908cfbc337434
felix <felixfernando232@gmail.com>
2022-01-11 01:54:41 +0700

    pagination

--- pages/stocks.php
+++ pages/stocks.php
@@ -9,8 +9,7 @@
   <meta name="author" content="" />
 
   <title>NAHINI Warehouse</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
 
   <!-- Bootstrap Core CSS -->
   <link href="../css/bootstrap.min.css" rel="stylesheet" />
