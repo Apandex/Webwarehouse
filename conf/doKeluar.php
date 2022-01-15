b97098686977312281b950eaf75eb6d895f0b777
Riski <rizkir42@gmail.com>
2022-01-16 00:39:01 +0700

    fixed bug

--- conf/doKeluar.php
+++ conf/doKeluar.php
@@ -5,13 +5,14 @@ include 'connection.php';
 $id = $_POST['id'];
 $nama = $_POST['prod_Name'];
 $qty = $_POST['prod_Qty'];
-$jumlah =  $qty - $_POST['Out_Qty'];
+$outqty = $_POST['Out_Qty'];
+$jumlah =  $qty - $outqty;
 
 
 // update data ke database
 mysqli_query($conn, "update inventory set  prod_Qty='$jumlah' where id='$id'");
 
-mysqli_query($conn, "insert into history(data,date) values('Barang Keluar nama = $nama, qty = $jumlah', curdate() )");
+mysqli_query($conn, "insert into history(data,date) values('Barang Keluar nama = $nama, qty = $outqty', curdate() )");
 
 
 header("location:../pages/stocks.php");
