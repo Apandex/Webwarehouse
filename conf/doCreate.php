c4e94516c7ef31725733e39622f90f5e7d5118dc
Riski <rizkir42@gmail.com>
2022-01-16 21:04:23 +0700

    prepared statement

--- conf/doCreate.php
+++ conf/doCreate.php
@@ -9,26 +9,47 @@ $jumlah = $_POST['prod_Qty'];
 $harga = $_POST['prod_Cost'];
 
 // menginput data ke database
-$sql = "SELECT * FROM inventory WHERE prod_Code='$code'";
-$result = mysqli_query($conn, $sql);
+$sql = "SELECT * FROM inventory WHERE prod_Code=?"; // SQL with parameters
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $code);
+$stmt->execute();
+$result = $stmt->get_result(); // get the mysqli result
 if (!$result->num_rows > 0) {
-    mysqli_query($conn, "insert into inventory (prod_Code, prod_Name, prod_Desc, prod_Qty,prod_Cost) values('$code','$nama','$deskripsi','$jumlah','$harga')");
+    $sqlinput = "insert into inventory (prod_Code, prod_Name, prod_Desc, prod_Qty,prod_Cost) values(?,?,?,?,?)";
+    // SQL with parameters
+    $stmtinput = $conn->prepare($sqlinput);
+    $stmtinput->bind_param("sssii", $code, $nama, $deskripsi, $jumlah, $harga);
+    $data = "Data Baru nama = $nama, deskripsi = $deskripsi, qty = $jumlah, harga = $harga";
+    $date = date("Y-m-d");
 
-    mysqli_query($conn, "insert into history(data,date) values('Data Baru nama = $nama, deskripsi = $deskripsi, qty = $jumlah, harga = $harga', curdate() )");
-    $code = "";
-    $nama = "";
-    $deskripsi = "";
-    $jumlah = "";
-    $harga = "";
+    if ($stmtinput->execute()) {
+
+        $sql1 = "insert into history (data, date) values(?,?)";
+        $stmthistory = $conn->prepare(($sql1));
+        echo $data;
+        echo $date;
+        $stmthistory->bind_param("ss", $data, $date);
+        if ($stmthistory->execute()) {
+            $code = "";
+            $nama = "";
+            $deskripsi = "";
+            $jumlah = "";
+            $harga = "";
+            echo "<script>
+        alert('data berhasil direkam');
+        window.location.href='../pages/stocks.php';
+        </script>";
+        } else {
+            echo $stmthistory->error;
+        };
+    } else {
+        echo $stmtinput->error;
+    }
+} else {
     echo "<script>
-    alert('data berhasil direkam');
-    window.location.href='../pages/stock.php';
-    </script>";} 
-    else {
-    echo "<script>
-alert('Product sudah terdaftar');
-window.location.href='../pages/goods.php';
-</script>";
+    alert('Product sudah terdaftar');
+    window.location.href='../pages/goods.php';
+    </script>";
     $code = "";
     $nama = "";
     $deskripsi = "";
